d4b20292ba220ab6552344242e185f1cda53a4f2
Luke Davis <XLTechie+github@newanswertech.com>
2023-03-09 02:57:54 -0500

    Version 23.1.0.
    Included (with permission, and with many modifications and updates) the numlock control code from the legacy NumLock Manager add-on by Noelia Ruiz (@nvdaes).
    That lead to the following features:
    - A configuration option to do nothing with numlock, always turn it off, or always turn it on.
    - If numlock is set to turn off or on when NVDA starts, its state is restored when NVDA exits.
    - A checkbox (only enabled in the normal profile), to determine whether the initial numlock state is applied to configuration profiles.
    - By default, profiles are ignored for numlock state changes.
    
    In addition, this version:
    - Fixed a translator comment.
    - Updated the __init__ docstring for the GlobalPlugin.
    - Added logging of numlock state and changes at various points.
    - Updated readme with new features and a partial changelog.
    - Updated and simplified description in buildVars.
    - Incorporated latest changes from add-on template.

--- buildVars.py
+++ buildVars.py
@@ -23,12 +23,10 @@ addon_info = {
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Allows switching the numpad between NVDA navigation mode, and Windows navigation mode.
-This can be especially useful for laptop users who prefer laptop mode, but who have a numpad and want to use it for arrow keys, page up/down, and home/end.
-It may also be valuable for desktop users coming from JAWS, who prefer commands like NumpadInsert+Numpad2 for read to end.
-And since the mode can be changed with a keypress (Alt+NVDA+NumpadPlus, by default), the numpad can easily be used for object navigation or review when desired.
-Feature requests, use-cases, and feedback welcome."""),
+Especially useful if migrating from Jaws; or for laptop users preferring laptop mode, but who have a numpad and want to use it for arrow keys, etc.
+The add-on also gives control of numlock state when starting NVDA or changing profiles.."""),
 	# version
-	"addon_version": "23.0",
+	"addon_version": "23.1.0",
 	# Author(s)
 	"addon_author": "Luke Davis (Open Source Systems, Ltd.) <XLTechie@newanswertech.com>",
 	# URL for the add-on documentation support
--- todo.md
+++ todo.md
@@ -7,3 +7,4 @@
 * Add config checkboxes to follow or not config profile changes, maybe separately for numlock and nav mode.
 * Refactor key handling so that all modifiers are possible.
 * Switch from namedtuple to a metadata class.
+* Investigate numlockManager's onHelp, and re-focusing code, to see if it can be useful.

1ebd0bc9a04e64b3aa8994b8235d92e83c36b70b
Luke Davis <XLTechie+github@newanswertech.com>
2023-06-02 21:24:50 -0400

    Workflow: Update builder to build under Python 3.7, and build and upload the .pot file.

--- buildVars.py
+++ buildVars.py
@@ -26,7 +26,7 @@ addon_info = {
 Especially useful if migrating from Jaws; or for laptop users preferring laptop mode, but who have a numpad and want to use it for arrow keys, etc.
 The add-on also gives control of numlock state when starting NVDA or changing profiles.."""),
 	# version
-	"addon_version": "23.1.3",
+	"addon_version": "23.1.4",
 	# Author(s)
 	"addon_author": "Luke Davis (Open Source Systems, Ltd.) <XLTechie@newanswertech.com>",
 	# URL for the add-on documentation support

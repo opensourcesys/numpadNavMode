e3de260ff1d934bd2f80fbce7a37223e192ef18b
Luke Davis <xltechie+github@newanswertech.com>
2026-05-01 08:53:45 +0000

    NVDA 2026.1 compatibility.

--- buildVars.py
+++ buildVars.py
@@ -25,11 +25,10 @@ addon_info = AddonInfo(
 Especially useful if migrating from Jaws; or for laptop users preferring laptop mode, but who have a numpad and want to use it for arrow keys, etc.
 The add-on also gives control of numlock state when starting NVDA or changing profiles."""),
 	# version
-	addon_version="26.1.5",
+	addon_version="26.2.0",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""* NVDA 2026.1 ready.
-* Made the add-on code more robust with help from Pyright (a Python static type checker)."""),
+	addon_changelog=_("""* NVDA 2026.1 compatible."""),
 	# Author(s)
 	addon_author="Luke Davis <XLTechie@newanswertech.com>, Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
@@ -41,7 +40,7 @@ The add-on also gives control of numlock state when starting NVDA or changing pr
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2019.3.0",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.3.3",
+	addon_lastTestedNVDAVersion="2026.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
